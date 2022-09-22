3b478c70b05bb3188efcba7dfd2438b669f371ea
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2022-09-22 12:16:42 +0100

    <"findEvenIndex">

---
+++ Even number.js
@@ -0,0 +1,21 @@
+//You are going to be given an array of integers. 
+//Your job is to take that array and find an index N where the sum of the integers to the left of N is equal 
+//to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+
+
+
+
+
+function findEvenIndex(arr)
+{
+  let i;
+  for(i = 0; i < arr.length; i++){
+      let slic1 = arr.slice(0,i).reduce((num1, num2) => num1 + num2, 0);
+      let slic2 = arr.slice((i + 1), arr.length).reduce((num1, num2) => num1 + num2, 0);
+      if(slic1 == slic2){
+        return i;
+      }
+  }
+  return -1
+}
