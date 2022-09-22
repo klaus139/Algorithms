84f369df5f80a8e3d15853e1d0f227e6fecf02e3
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2022-09-22 12:24:14 +0100

    <"even number">

--- Even number.js
+++ Even number.js
@@ -19,3 +19,14 @@ function findEvenIndex(arr)
   }
   return -1
 }
+
+//another method to solve this problem
+let left = 0;
+  let right = arr.reduce((s,n) => s + n, 0);
+  for (let i = 0; i < arr.length; i++) {
+    right -= arr[i];
+    if (left === right) return i;
+    left += arr[i];
+  }
+  return -1;
+//this is easier in my opinion
