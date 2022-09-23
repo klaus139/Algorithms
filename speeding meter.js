de87905cbd99f06006ddf3de4b821e860c01bc2b
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2022-09-23 08:27:50 +0100

    <"count strings">

--- speeding meter.js
+++ speeding meter.js
@@ -12,3 +12,43 @@ function checkSpeed(speed){
     }
 }
 checkSpeed(130);
+
+function grabFruit(){
+    return ['apple','oranges','banana','cocoa','pineapple']
+}
+
+let [x,y,z,...args] = grabFruit();
+
+console.log(args);
+
+function stat(a, b) {
+    return[
+        a  + b,
+        (a + b ) / 2,
+        a - b
+
+    ]
+}
+
+//destrucring
+let[sum, average, minus] = stat(10, 20);
+console.log(average);
+
+
+
+
+function count (string){
+    let obj = {};
+    for (let i = 0; i < string.length; i++){
+        let char = string[i];
+
+        if (obj.includes(string[i])){
+            obj[string[i]]++;
+
+        } else {
+            obj[string[i]] = 1;
+        }
+    }
+    return obj;
+}
+console.log(count('hello'));
